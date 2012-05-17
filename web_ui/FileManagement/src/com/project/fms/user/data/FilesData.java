ab9b3b90b5c5d48ffbff5d8e1e50bb57d986f698
deadlydeo <deepakmdeo@gmail.com>
2012-05-17 17:43:55 +0530

    Up to Transcriber UI Screens

--- web_ui/FileManagement/src/com/project/fms/user/data/FilesData.java
+++ web_ui/FileManagement/src/com/project/fms/user/data/FilesData.java
@@ -12,14 +12,14 @@ public class FilesData {
   
     public static FileRecord[] getNewRecords() {  
         return new FileRecord[]{
-        		new FileRecord("File 1", "F001", "0"),//Open for Transcriber
-        		new FileRecord("File 2", "F002", "1"),//Closed for transcriber, open for Editor
-        		new FileRecord("File 3", "F003", "2"),//Open for transcriber(returned by editor),closed for Editor
-        		new FileRecord("File 4", "F004", "3"),//Closed for transcriber, Closed for Editor, Open for Proofer.
-        		new FileRecord("File 5", "F005", "4"),//Closed for Transcriber, Open for Editor(returned by Proofer),closed for Proofer
-        		new FileRecord("File 6", "F006", "5"),//Closed for Transcriber, Closed for Editor, Closed for Proofer, Open for QA
-        		new FileRecord("File 7", "F007", "6"),//Closed for Transcriber, Closed for Editor, Open for Proofer(returned by QA), Closed for QA 
-        		new FileRecord("File 8", "F008", "7"),//File Processing Finished
+        		new FileRecord("File 8", "F001", "0"),//Open for Transcriber
+        		new FileRecord("File 7", "F002", "1"),//Closed for transcriber, open for Editor
+        		new FileRecord("File 6", "F003", "2"),//Open for transcriber(returned by editor),closed for Editor
+        		new FileRecord("File 5", "F004", "3"),//Closed for transcriber, Closed for Editor, Open for Proofer.
+        		new FileRecord("File 4", "F005", "4"),//Closed for Transcriber, Open for Editor(returned by Proofer),closed for Proofer
+        		new FileRecord("File 3", "F006", "5"),//Closed for Transcriber, Closed for Editor, Closed for Proofer, Open for QA
+        		new FileRecord("File 2", "F007", "6"),//Closed for Transcriber, Closed for Editor, Open for Proofer(returned by QA), Closed for QA 
+        		new FileRecord("File 1", "F008", "7"),//File Processing Finished
         		
         };  
     }  
